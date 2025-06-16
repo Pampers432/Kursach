ac4c2d11d3792dcc36248b37d51e7839c867fd9d
Pampers432 <max23012007@gmail.com>
2025-06-16 16:16:02 +0300

    Добавьте файлы проекта.

---
+++ Kursach/Classes.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kursach
+{
+    public class Village
+    {
+        [Key]
+        public int Id { get; set; }
+        [ForeignKey("Location")]
+        public int Location_Id { get; set; }
+        public string Name { get; set; }
+        public string PopulationBefore { get; set; }
+        public DateTime DateDestroyed { get; set; }
+        public string Cause { get; set; }
+        public bool MemorialExists { get; set; }
+        public string? ImagePath { get; set; }
+        public string Description { get; set; }
+        public Village() { }
+    }
+
+    public class Location
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Region { get; set; }
+        public string District { get; set; }
+        public string Latitude { get; set; }
+        public string Longitude { get; set; }
+
+        public Location() { }
+    }
+
+    public class Monument
+    {
+        [Key]
+        public int Id { get; set; }
+        [ForeignKey("Location")]
+        public int Location_Id { get; set; }
+        public string Name { get; set; }
+        public DateTime InstallationDate { get; set; }
+        public string Cause { get; set; }
+        public string? ImagePath { get; set; }
+        public string Description { get; set; }
+
+        public Monument() { }
+    }
+
+    public class Ghetto
+    {
+        [Key]
+        public int Id { get; set; }
+        [ForeignKey("Location")]
+        public int Location_Id { get; set; }
+        public string Name { get; set; }
+        public DateTime EstablishedDate { get; set; }
+        public DateTime LiquidationDate { get; set; }
+        public int VictimsCount { get; set; }
+        public int Population { get; set; }
+        public string? ImagePath { get; set; }
+        public string Description { get; set; }
+
+        public Ghetto() { }
+    }
+
+    public class MassGrave
+    {
+        [Key]
+        public int Id { get; set; }
+        [ForeignKey("Location")]
+        public int Location_Id { get; set; }
+        public string Name { get; set; }
+        public DateTime InstallationDate { get; set; }
+        public string Cause { get; set; }
+        public int VictimsCount { get; set; }
+        public string? ImagePath { get; set; }
+        public string Description { get; set; }
+
+        public MassGrave() { }
+    }
+
+    public class Victim
+    {
+        [Key]
+        public int Id { get; set; }
+        [ForeignKey("MassGrave")]
+        public int MassGrave_Id { get; set; }
+        public string LastName { get; set; }
+        public string Name { get; set; }
+        public string MiddleName { get; set; }
+        public DateTime BirthDate { get; set; }
+        public DateTime DeathDate { get; set; }
+        public string? ImagePath { get; set; }
+        public string Description { get; set; }
+
+        public Victim() { }
+    }
+}
---
+++ Kursach/MyDBContext.cs
@@ -0,0 +1,110 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.IO;
+
+namespace Kursach
+{
+    public class MyDBContext : DbContext
+    {
+        public DbSet<Village> Villages { get; set; }
+        public DbSet<Location> Locations { get; set; }
+        public DbSet<Monument> Monuments { get; set; }
+        public DbSet<Ghetto> Ghettos { get; set; }
+        public DbSet<MassGrave> MassGraves { get; set; }
+        public DbSet<Victim> Victims { get; set; }
+
+        private const string DatabaseName = "KursachDB.db";
+        private static string DatabasePath => Path.Combine(Directory.GetCurrentDirectory(), DatabaseName);
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlite($"Data Source={DatabasePath}");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            // Отключает каскадное удаление по умолчанию
+            foreach (var relationship in modelBuilder.Model.GetEntityTypes().SelectMany(e => e.GetForeignKeys()))
+            {
+                relationship.DeleteBehavior = DeleteBehavior.Restrict;
+            }
+        }
+
+        // Для создания БД и таблицы, если их нет
+        public static void InitializeDatabase()
+        {
+            using (var db = new MyDBContext())
+            {
+                db.Database.EnsureCreated(); 
+            }
+        }
+
+
+        public static async Task CreateTablesAsync(Location location, Village village, Monument monument,
+                                                   Ghetto ghetto, MassGrave massGrave, Victim victim)
+        {
+            await CreateLocationAsync(location);
+            await CreateVillageAsync(village);
+            await CreateMonumentAsync(monument);
+            await CreateGhettoAsync(ghetto);
+            await CreateMassGraveAsync(massGrave);
+            await CreateVictimAsync(victim);
+        }
+
+        public static async Task CreateLocationAsync(Location location)
+        {
+            using (var db = new MyDBContext())
+            {
+                await db.Locations.AddAsync(location);
+                await db.SaveChangesAsync();
+            }
+        }
+
+        public static async Task CreateVillageAsync(Village village)
+        {
+            using (var db = new MyDBContext())
+            {
+                await db.Villages.AddAsync(village);
+                await db.SaveChangesAsync();
+            }
+        }
+
+        public static async Task CreateMonumentAsync(Monument monument)
+        {
+            using (var db = new MyDBContext())
+            {
+                await db.Monuments.AddAsync(monument);
+                await db.SaveChangesAsync();
+            }
+        }
+
+        public static async Task CreateGhettoAsync(Ghetto ghetto)
+        {
+            using (var db = new MyDBContext())
+            {
+                await db.Ghettos.AddAsync(ghetto);
+                await db.SaveChangesAsync();
+            }
+        }
+
+        public static async Task CreateMassGraveAsync(MassGrave massGrave)
+        {
+            using (var db = new MyDBContext())
+            {
+                await db.MassGraves.AddAsync(massGrave);
+                await db.SaveChangesAsync();
+            }
+        }
+
+        public static async Task CreateVictimAsync(Victim victim)
+        {
+            using (var db = new MyDBContext())
+            {
+                await db.Victims.AddAsync(victim);
+                await db.SaveChangesAsync();
+            }
+        }
+    }
+}
